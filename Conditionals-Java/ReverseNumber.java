d44daba532c46a8682d60e61106dc1f9d8673a1d
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-02-14 17:25:47 +0530

    Update ReverseNumber.java

--- Conditionals-Java/ReverseNumber.java
+++ Conditionals-Java/ReverseNumber.java
@@ -3,15 +3,29 @@ import java.util.Scanner;
 public class ReverseNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        
+//      Declaring a variable res and assigning value 0 to it.
+        
         int res = 0;
+        
+//      Taking the input from the user.
+        
         int n = input.nextInt();
+        
+//      Using while loop with condition (n > 0).
 
         while (n > 0) {
+//      This part is purely mathematical. But still i would like to elaborate.
+//      Firstly, obtaining the remainder after dividing n with 10. The value of the remainder will be assigned to variable rem.
+//      After each iteration we have to divide n by 10.
             int rem = n % 10;
             n /= 10;
             res = (res * 10) + rem;
+//      For this problem, I would like you to do it manually in pen and paper mode.
+//      It's not that hard to understand. For example - Take a number 134.
         }
         System.out.println(res);
     }
 }
+
+// Rituraj Mahato
