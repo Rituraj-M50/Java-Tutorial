b3d81bcfbff0cf2f4b5125b7c488d2789241a47d
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-02-14 11:42:56 +0530

    Create Sum.java

---
+++ Introduction to Java - Basics/Sum.java
@@ -0,0 +1,21 @@
+// This example is quite simple and self-explainatory.
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum will be : " + sum);
+    }
+}
+
+// You have to declare the variables before using them in Java Language.
+// Declaration of a variable means letting the JVM know that what kind of data we would be dealing with.
+// For example- String for string values, int for integer values, char for character value, etc.
+
+// One more thing, in Java try to use Double Quotes("") to represent strings and Single Quotes('') to represent characters.
+
+// Rituraj Mahato
