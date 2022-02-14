d8dc3d5e5d2cd2c55aac9197097a994e73b04d01
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-02-14 11:35:40 +0530

    Create Primitives.java

---
+++ Introduction to Java - Basics/Primitives.java
@@ -0,0 +1,25 @@
+// Primitives means the datatypes which cannot be divided further.
+
+public class Primitives {
+    public static void main(String[] args) {
+//       String datatype is non-primitive as it's a sequence of characters.
+//       Strings are immutable, do to which it enhances the security and efficiency by allowing pooling.
+      
+//       String pool is nothing but a storage area in Java heap where string literals stores.
+//       It is also known as String Intern Pool or String Constant Pool. It is just like object allocation.
+//       By default, it is empty and privately maintained by the Java String class.
+      
+//       These are the most common primitive datatypes in Java -
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumber = 456353.2982;
+        long largeInteger = 27328327638272L;
+        boolean check = false;
+      
+//       The eight primitives defined in Java are int, byte, short, long, float, double, boolean, and char.
+//       Those aren't considered objects and represent raw values.
+    }
+}
+
+// Rituraj Mahato
