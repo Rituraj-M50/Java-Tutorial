66f6c3b72adc0e29a7d434bdf9d332ba96917e90
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-02-14 11:26:01 +0530

    Create Inputs.java

---
+++ Introduction to Java - Basics/Inputs.java
@@ -0,0 +1,28 @@
+// java.util.Scanner is a utility class already available in JVM (Java Virtual Machine)
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+//       Scanner class is used to take input from the Keyboard, Hence we use System.in
+      
+//       Always remember to put a ';' at the end.
+      
+        Scanner input = new Scanner(System.in);
+      
+//       Use println to print the next output in new line.
+//       Use print to print the next output in same line.
+      
+        System.out.println("Please enter some input:");
+      
+//       Taking the input from the user.
+      
+        int roll_no = input.nextInt();
+     
+//       We have things like nextInt(), nextLine() and many other.
+//       Visit this link to know more about Scanner Class in Java - https://www.jigsawacademy.com/blogs/java/scanner-class-in-java/
+     
+        System.out.println("Your roll_no is " + roll_no);
+     
+    }
+}
