64f6bd8720df2f3dd63081c1eb37e304c1146a48
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-02-14 11:10:42 +0530

    Create Basics.java

---
+++ Introduction to Java - Basics/Basics.java
@@ -0,0 +1,30 @@
+public class Basics {
+    public static void main(String[] args) {
+
+// if and else Statements in Java
+// You should use lowercase for if and else
+
+      int a = 10;
+       if (a == 12){
+           System.out.println("Hello World");
+       }
+       else{
+           System.out.println("Bye World");
+       }
+
+// while loop in Java
+
+       int count = 1;
+       while (count != 5){
+           System.out.println(count);
+           count += 1;
+       }
+
+// for loop in Java
+      
+        for (int count = 1; count != 5; count ++){
+            System.out.println(count);
+        }
+      
+    }
+}
